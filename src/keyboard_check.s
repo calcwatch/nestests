103eacfae5cfdfa9f8989bd26ba6572d210a1ed4
Richard Calvi <calcwatch@gmail.com>
2022-03-02 22:38:43 +0900

    More style cleanup; lowercased opcodes

--- src/keyboard_check.s
+++ src/keyboard_check.s
@@ -22,10 +22,10 @@ SCREEN_WIDTH = $20
 
 ; Prepares PPUDATA register for writes, starting at the supplied address
 .macro set_ppu_addr addr
-    LDA #(>addr)
-    STA PPUADDR
-    LDA #(<addr)
-    STA PPUADDR
+    lda #(>addr)
+    sta PPUADDR
+    lda #(<addr)
+    sta PPUADDR
 .endmacro
 
 ; Sets the PPU up to write to the nametable at the specified column and row
@@ -35,13 +35,13 @@ SCREEN_WIDTH = $20
 
 .macro write_from_buffer col, row, start, end
     move_cursor col, row
-    LDY #(end - start)
+    ldy #(end - start)
 .scope
     @string_print_loop:
-    LDA screen_buffer + keyboard_row_end - end - 1, y
-    STA PPUDATA
-    DEY
-    BNE @string_print_loop
+    lda screen_buffer + keyboard_row_end - end - 1, y
+    sta PPUDATA
+    dey
+    bne @string_print_loop
 .endscope
 .endmacro
 
@@ -49,11 +49,11 @@ SCREEN_WIDTH = $20
 ; Writes the zero-terminated ASCII string at the specified address to the
 ; nametable
 .macro write_string addr
-    LDA #(<addr)
-    STA puts_string_pointer
-    LDA #(>addr)
-    STA puts_string_pointer + 1
-    JSR puts
+    lda #(<addr)
+    sta puts_string_pointer
+    lda #(>addr)
+    sta puts_string_pointer + 1
+    jsr puts
 .endmacro
 
 ; Produces ASCII string with bit 7 high on the last character
@@ -180,21 +180,21 @@ palette_data:
 .segment "CODE"
 
 nmi:
-    PHA                             ; save A
-    TXA                             ; copy X
-    PHA                             ; save X
-    TYA                             ; copy Y
-    PHA                             ; save Y
+    pha                             ; save A
+    txa                             ; copy X
+    pha                             ; save X
+    tya                             ; copy Y
+    pha                             ; save Y
 
-    LDA soft_ppu_mask
-    STA PPUMASK
+    lda soft_ppu_mask
+    sta PPUMASK
 
-    LDA buffer_ready
-    BNE print_from_buffer
-    JMP end_of_nmi
+    lda buffer_ready
+    bne print_from_buffer
+    jmp end_of_nmi
 
 print_from_buffer:
-    LDY #$00
+    ldy #$00
 
     ; I stamp out the string writing routine each time so that
     ; it can just _barely_ finish before the vblank ends.
@@ -210,74 +210,74 @@ print_from_buffer:
     write_from_buffer 7, 25, keyboard_row_8, keyboard_row_end
 
 end_of_nmi:
-    LDA #$00
-    STA PPUSCROLL    ; Set x & y scroll positions to 0
-    STA PPUSCROLL
+    lda #$00
+    sta PPUSCROLL    ; Set x & y scroll positions to 0
+    sta PPUSCROLL
 
-    PLA                             ; pull Y
-    TAY                             ; restore Y
-    PLA                             ; pull X
-    TAX                             ; restore X
-    PLA                             ; restore A
-    RTI
+    pla                             ; pull Y
+    tay                             ; restore Y
+    pla                             ; pull X
+    tax                             ; restore X
+    pla                             ; restore A
+    rti
 
 ; Not used
 irq:
-    RTI
+    rti
 
 reset:
-    SEI
-    CLD            ; Disable binary-encoded decimal support, by convention
+    sei
+    cld            ; Disable binary-encoded decimal support, by convention
         
-    LDA #$00
-    STA PPUCTRL    ; Disable NMIs
-    STA PPUMASK    ; turn PPU off
-    STA $4010      ; Disable DMC IRQs
-    LDA #$C0
-    STA $4017
+    lda #$00
+    sta PPUCTRL    ; Disable NMIs
+    sta PPUMASK    ; turn PPU off
+    sta $4010      ; Disable DMC IRQs
+    lda #$C0
+    sta $4017
 
-    LDX #$02       ; Wait for 2 vblanks so that the PPU can warm up
+    ldx #$02       ; Wait for 2 vblanks so that the PPU can warm up
 @wait_for_ppu:
-    BIT PPUSTATUS
-    BPL @wait_for_ppu
-    DEX
-    BNE @wait_for_ppu
+    bit PPUSTATUS
+    bpl @wait_for_ppu
+    dex
+    bne @wait_for_ppu
 
-    LDX #$FF            ; set X for stack
-        TXS             ; clear stack
+    ldx #$FF            ; set X for stack
+    txs             ; clear stack
 
     ; clear RAM
-    LDA #$00
-    LDX #$00
+    lda #$00
+    ldx #$00
 zero_out_loop:
-    STA $00, x
-    STA $200, x
+    sta $00, x
+    sta $200, x
     STA $300, x
-    STA $400, x
-    STA $500, x
-    STA $600, x
-    STA $700, x
-    INX
-    BNE zero_out_loop
+    sta $400, x
+    sta $500, x
+    sta $600, x
+    sta $700, x
+    inx
+    bne zero_out_loop
     
     ; set so that the first inc in the NMI will make it zero
-    LDA #$FF
-    STA key_checked
+    lda #$FF
+    sta key_checked
 
-    LDA PPUSTATUS   ; read PPU status to reset the high/low latch to high
+    lda PPUSTATUS   ; read PPU status to reset the high/low latch to high
 
     ; clear nametable
     set_ppu_addr NAMETABLE_START
-    LDA #' ' ; character to write
-    LDY #(>NAMETABLE_SIZE) ; # of pages to write to
+    lda #' ' ; character to write
+    ldy #(>NAMETABLE_SIZE) ; # of pages to write to
 write_page:
-    LDX #$00
+    ldx #$00
 write_byte:
-    STA PPUDATA
-    INX
-    BNE write_byte
-    DEY
-    BNE write_page
+    sta PPUDATA
+    inx
+    bne write_byte
+    dey
+    bne write_page
 
     ; Write title and info text
     move_cursor 5, 3
@@ -301,159 +301,159 @@ write_byte:
         ; set palettes
     set_ppu_addr PALETTE_RAM_START
 
-    LDX #$00
+    ldx #$00
 
 palette_loop:
-    LDA palette_data, x
-    STA PPUDATA    ; Write palette color to PPU
-    INX
-    CPX #PALETTE_RAM_SIZE
-    BNE palette_loop
+    lda palette_data, x
+    sta PPUDATA    ; Write palette color to PPU
+    inx
+    cpx #PALETTE_RAM_SIZE
+    bne palette_loop
 
-    LDA #$00
-    STA PPUSCROLL    ; Set x & y scroll positions to 0
-    STA PPUSCROLL
+    lda #$00
+    sta PPUSCROLL    ; Set x & y scroll positions to 0
+    sta PPUSCROLL
 
-    LDA #$0e
-    STA soft_ppu_mask ; Enable backgrounds on next vblank, but not sprites
+    lda #$0E
+    sta soft_ppu_mask ; Enable backgrounds on next vblank, but not sprites
 
-    LDA #$80
-    STA PPUCTRL ; Enable NMI on vblank
+    lda #$80
+    sta PPUCTRL ; Enable NMI on vblank
 
-    CLI         ; Enable the interrupts
+    cli         ; Enable the interrupts
 
 infinite_loop:
-    LDY #$00; keyboard row count
+    ldy #$00; keyboard row count
 
-    LDA #$05    ; reset code
-    STA $4016   ; reset keyboard scan to row 0, column 0
+    lda #$05    ; reset code
+    sta $4016   ; reset keyboard scan to row 0, column 0
 key_row_scan_loop:
-    LDA #$04   ; "next row" code
-    STA $4016  ; select column 0, next row if not just reset
-    LDX #ROW_LOAD_DELAY
+    lda #$04   ; "next row" code
+    sta $4016  ; select column 0, next row if not just reset
+    ldx #ROW_LOAD_DELAY
 @wait_for_row:
-    DEX
-    BNE @wait_for_row
+    dex
+    bne @wait_for_row
 
-    LDA $4017  ; read column 0 data
+    lda $4017  ; read column 0 data
 
     ; do stuff with col 0
     LSR A; slide it to the right to knock off bit 0 (a "don't care")
-    AND #$0F ; knock off bits we don't care about
-    STA key_scratch_space, y ; store in temp space for now
+    and #$0F ; knock off bits we don't care about
+    sta key_scratch_space, y ; store in temp space for now
 
-    LDA #$06   ; "next column" code
-    STA $4016  ; select column 1
-    LDX #ROW_LOAD_DELAY
+    lda #$06   ; "next column" code
+    sta $4016  ; select column 1
+    ldx #ROW_LOAD_DELAY
 @wait_for_column:
-    DEX
-    BNE @wait_for_column
+    dex
+    bne @wait_for_column
 
-    LDA $4017  ; read column 1 data
+    lda $4017  ; read column 1 data
 
-    LDX     #$08    ; set the column count
+    ldx #$08    ; set the column count
 
     ; do stuff with col 1
-    ASL a
-    ASL a
-    ASL a
-    AND #$f0 ; knock off bits we don't care about
-    ORA key_scratch_space, y ; join it with the bits from col 0
-    STA key_scratch_space, y
-    INY
+    asl a
+    asl a
+    asl a
+    and #$f0 ; knock off bits we don't care about
+    ora key_scratch_space, y ; join it with the bits from col 0
+    sta key_scratch_space, y
+    iny
 
-    CPY #KEYBOARD_MATRIX_ROW_COUNT
-    BNE key_row_scan_loop
+    cpy #KEYBOARD_MATRIX_ROW_COUNT
+    bne key_row_scan_loop
 
 ; Screen buffer writing: It writes the buffer out backwards so that the
 ; print loops can decrement Y until Y==0, saving the need for an X iterator
-    LDA #(<keyboard_strings)
-    STA string_list_pointer
-    LDA #(>keyboard_strings)
-    STA string_list_pointer+1
+    lda #(<keyboard_strings)
+    sta string_list_pointer
+    lda #(>keyboard_strings)
+    sta string_list_pointer+1
 
-    LDY #$00
+    ldy #$00
 string_list_loop:
-    CPY #(keyboard_row_end - keyboard_strings)
-    BEQ done_with_strings
+    cpy #(keyboard_row_end - keyboard_strings)
+    beq done_with_strings
 
-    INC key_checked
+    inc key_checked
 
-    LDX key_checked
-    LDA matrix_index_to_screen_order, x
-    LSR
-    LSR
-    LSR
-    AND #$1F
-    TAX
-    LDA key_scratch_space, x
-    STA temp_buffer_write_byte
-    LDX key_checked
-    LDA matrix_index_to_screen_order, x
-    AND #$07
-    TAX
-    LDA temp_buffer_write_byte
-    AND and_bits, x
-    BEQ @set_key_hit
-    LDA #$00
-    STA key_hit
-    BEQ @load_string
+    ldx key_checked
+    lda matrix_index_to_screen_order, x
+    lsr
+    lsr
+    lsr
+    and #$1F
+    tax
+    lda key_scratch_space, x
+    sta temp_buffer_write_byte
+    ldx key_checked
+    lda matrix_index_to_screen_order, x
+    and #$07
+    tax
+    lda temp_buffer_write_byte
+    and and_bits, x
+    beq @set_key_hit
+    lda #$00
+    sta key_hit
+    beq @load_string
 @set_key_hit:
-    LDA #$80
-    STA key_hit
-    BNE @load_string
+    lda #$80
+    sta key_hit
+    bne @load_string
 @load_string:
-    LDA keyboard_strings, y
+    lda keyboard_strings, y
 string_write_loop:
-    STA temp_buffer_write_byte
-    TYA         ; A = Y
-    EOR #$FF    ; A = -Y - 1
-    CLC
-    ADC #(keyboard_row_end - keyboard_strings) ; A = buffer_length - Y - 1
-    TAX                                        ; X = buffer_length - Y - 1
-    LDA temp_buffer_write_byte
-    AND #$7F
-    ORA key_hit
-    STA screen_buffer, x
-    INY
-    LDA temp_buffer_write_byte
-    BMI string_list_loop  ; byte with b7==1 means it's the last char of the string
-    LDA keyboard_strings, y
-    BNE string_write_loop ; branch always, since bytes in string list should never be 0
+    sta temp_buffer_write_byte
+    tya         ; A = Y
+    eor #$FF    ; A = -Y - 1
+    clc
+    adc #(keyboard_row_end - keyboard_strings) ; A = buffer_length - Y - 1
+    tax                                        ; X = buffer_length - Y - 1
+    lda temp_buffer_write_byte
+    and #$7F
+    ora key_hit
+    sta screen_buffer, x
+    iny
+    lda temp_buffer_write_byte
+    bmi string_list_loop  ; byte with b7==1 means it's the last char of the string
+    lda keyboard_strings, y
+    bne string_write_loop ; branch always, since bytes in string list should never be 0
 done_with_strings:
-    LDA #$FF
-    STA key_checked
+    lda #$FF
+    sta key_checked
 
-    LDA #$01
-    STA buffer_ready
+    lda #$01
+    sta buffer_ready
 
-    JMP infinite_loop
+    jmp infinite_loop
 
 ; This prints a null-terminated string referenced at puts_string_pointer to
 ; the screen. By default, the text will be gray. The "`" character is used to
 ; toggle green text, and will not be printed. The string must be less than 256
 ; characters long.
 puts:
-    LDY #$00
-    STY puts_green_flag
+    ldy #$00
+    sty puts_green_flag
 @print_loop:
-    LDA (puts_string_pointer), y
-    BEQ @done_printing
-    CMP #'`'
-    BEQ @toggle_green
-    EOR puts_green_flag
-    STA PPUDATA
-    INY
-    BNE @print_loop ; should branch always, unless we hit 256 chars
-    BEQ @done_printing
+    lda (puts_string_pointer), y
+    beq @done_printing
+    cmp #'`'
+    beq @toggle_green
+    eor puts_green_flag
+    sta PPUDATA
+    iny
+    bne @print_loop ; should branch always, unless we hit 256 chars
+    beq @done_printing
 @toggle_green:
-    LDA #$80
-    EOR puts_green_flag
-    STA puts_green_flag
-    INY
-    BNE @print_loop ; should branch always, unless we hit 256 chars
+    lda #$80
+    eor puts_green_flag
+    sta puts_green_flag
+    iny
+    bne @print_loop ; should branch always, unless we hit 256 chars
 @done_printing:
-    RTS
+    rts
 
 .segment "VECTORS"
 
